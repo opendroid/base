2f93cf2107ffa99d4a7977b48a70f65f83eef03d
Ajay Thakur <ajaythakur1972@gmail.com>
2021-01-06 12:43:35 -0800

    Updated with animae

--- js/togglePrimaryNav.js
+++ js/togglePrimaryNav.js
@@ -1,10 +1,10 @@
 "use strict";
 
 /**
- * toggleHKNav: Method hides or shows the HTML navigation section id "nav-primary"
+ * togglePrimaryNav: Method hides or shows the HTML navigation section id "nav-primary"
  * Uses classes: nav.hide, hk-top-bar-secondary-no-margin and hk-main-no-margin
  */
-function toggleHKNav() {
+function togglePrimaryNav() {
   const classes = document.getElementById("nav-primary").classList;
   if (classes && classes.contains("hide")) { // Show navbar
     document.getElementById("nav-primary").setAttribute("class", "np");
